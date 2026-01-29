0decd2ae3658414ad7e6de04a41f5e8e28df42d8
XabierLopez <xlopeza24@fpsanturtzilh.eus>
2026-01-29 23:49:13 +0100

    3.atala

--- src/app/servicios/cuestionario.service.ts
+++ src/app/servicios/cuestionario.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 import { IPregunta } from './../interfaces/interfaces';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { GestionStorageService } from './gestion-storage.service';
 import { Injectable } from '@angular/core';
 
@@ -10,21 +10,43 @@ import { Injectable } from '@angular/core';
 })
 export class CuestionarioService {
   // Array para almacenar todas las preguntas del json. Recordad inicializar el array para evitar problemas
-
+  galderak: IPregunta[] = [];
   //Añadir los componentes y servicios que se necesitan
-  constructor() {
+  constructor(private http: HttpClient, private gestionStorage: GestionStorageService) {
     //Cargar los datos
   }
 
   // Método que devolverá un array de IPregunta, es decir, todas las preguntas del cuestionario en un array
-
+  getGalderak():Observable<IPregunta[]> { //honek galderak erakutsiko dituenez edozein lekutan, observablea asincronia kudeatzeko ondo. this.galderak berriz, soilik zerbitzu barneko propietatea da, ez da erakutsiko kanpoan, beraz ez da observable behar.
+    return of(this.galderak);
+  }
 
   // Recupera las preguntas de Storage. Si no hay ninguna almacenada, las lee del fichero
   // Controlar la asincronía.
-
+  async galderakKargatu(){
+    var galderakStorage=await this.gestionStorage.getObject('galderak');
+    if(galderakStorage && galderakStorage.length>0){
+      this.galderak=galderakStorage;
+    }else{
+      this.jsonetikIrakurriGalderak().subscribe(irakurritakoGalderak => {
+        this.galderak=irakurritakoGalderak.map(galdera => {
+          return {
+            ...galdera, // jsonean bai dauden propietateak kopiatu
+            // gainerako propietateak bete hutsik
+            respuestasIncorrectas: [],
+            intentos: 0,
+            acierto: false
+          }
+        })
+      })
+      this.galderakStoragenGorde();
+    }
+  }
 
   // Lee los datos de un fichero y los almacena en un array
-
+  jsonetikIrakurriGalderak():Observable<IPregunta[]>{
+    return this.http.get<IPregunta[]>('../../assets/datos/datos.json');
+  }
 
   // Abre una alerta con el enunciado de la pregunta y comprueba la respuesta
   // 1- En función de si es correcta o no, actualiza el estado de acierto.
@@ -34,5 +56,7 @@ export class CuestionarioService {
 
 
   // Almacenar el array de preguntas en Storage
-
+  galderakStoragenGorde(){
+    this.gestionStorage.setObject('galderak', this.galderak);
+  }
 }
