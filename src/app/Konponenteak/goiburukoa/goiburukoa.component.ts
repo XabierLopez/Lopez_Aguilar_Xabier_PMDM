258cb34f7f7b18eaf5fd34810508ac3c2db3ac7e
XabierLopez <xlopeza24@fpsanturtzilh.eus>
2026-01-29 21:59:08 +0100

    1.ariketa

---
+++ src/app/Konponenteak/goiburukoa/goiburukoa.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-goiburukoa',
+  templateUrl: './goiburukoa.component.html',
+  styleUrls: ['./goiburukoa.component.scss'],
+  standalone: false
+})
+export class GoiburukoaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
